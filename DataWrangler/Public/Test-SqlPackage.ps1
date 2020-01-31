bd91b409e7b285f22b7a8254d74212dfd645f353
david-rushton <david-rushton@outlook.com>
2020-01-31 19:26:29 +0000

    FIRST DRAFT: Test-SqlPackage

--- DataWrangler/Public/Test-SqlPackage.ps1
+++ DataWrangler/Public/Test-SqlPackage.ps1
@@ -1,21 +1,36 @@
 <#
 #>
 function Test-SqlPackage {
-    [CmdletBind()]
+    [CmdletBinding()]
     param (
 
     )
 
     Set-StrictMode -Version 'Latest'
 
+    [string[]]$sqlPackagePaths = @()
 
+
+    # Search Visual Studio paths
     #
-    # C:\Program Files\Microsoft SQL Server\150\DAC\bin
     # C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\130
     # C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\140
     # C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\150
-    #                                                     ---------
+    #                                                ---- ---------
     # VS mod?
     # VS 2017, etc
     #
+
+
+    # Search SQL Server Paths
+    $compatablityLevels = (Get-dwSqlServerVersionTable | Select-Object -Property 'CompatablityLevel').CompatablityLevel
+    foreach($compatablityLevel in $compatablityLevels) {
+        [string]$candidatePath = "C:\Program Files\Microsoft SQL Server\$compatablityLevel\DAC\bin\SqlPackage.exe"
+        if(Test-Path -Path $candidatePath) {
+            $sqlPackagePaths += $candidatePath
+        }
+    }
+
+
+    return (Get-Command $sqlPackagePaths)
 }
